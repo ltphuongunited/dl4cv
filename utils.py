72ce186b8a4d79ff8a65b79943b24ffc62ac32b6
jun <timothy.j.jo@gmail.com>
2023-10-19 19:08:27 +1000

    modify utils

---
+++ utils.py
@@ -0,0 +1,319 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import timm
+from tqdm.auto import tqdm
+from torchvision import datasets, transforms
+import random
+from sklearn.metrics import classification_report
+from PIL import Image
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import os
+from torch.optim.lr_scheduler import _LRScheduler
+import math 
+import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+
+seed=42
+
+def plot_transformed_images(image_paths, transform, n=3, seed=seed):
+    """Plots a series of random images from image_paths.
+
+    Will open n image paths from image_paths, transform them
+    with transform and plot them side by side.
+
+    Args:
+        image_paths (list): List of target image paths.
+        transform (PyTorch Transforms): Transforms to apply to images.
+        n (int, optional): Number of images to plot. Defaults to 3.
+        seed (int, optional): Random seed for the random generator. Defaults to 42.
+    """
+    random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(f)
+            ax[0].set_title(f"Original \nSize: {f.size}")
+            ax[0].axis("off")
+
+            # Transform and plot image
+            # Note: permute() will change shape of image to suit matplotlib
+            # (PyTorch default is [C, H, W] but Matplotlib is [H, W, C])
+            transformed_image = transform(f).permute(1, 2, 0)
+            ax[1].imshow(transformed_image)
+            ax[1].set_title(f"Transformed \nSize: {transformed_image.shape}")
+            ax[1].axis("off")
+
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+
+    
+
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               device):
+    # Put model in train mode
+    model.train()
+
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+
+    # Loop through data loader data batches
+    for batch, (X, y) in tqdm(enumerate(dataloader)):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate  and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Calculate and accumulate accuracy metric across all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+
+    # Adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+def val_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module,
+              device):
+    # Put model in eval mode
+    model.eval()
+
+    # Setup test loss and test accuracy values
+    val_loss, val_acc = 0, 0
+
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            val_pred_logits = model(X)
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(val_pred_logits, y)
+            val_loss += loss.item()
+
+            # Calculate and accumulate accuracy
+            val_pred_labels = val_pred_logits.argmax(dim=1)
+            val_acc += ((val_pred_labels == y).sum().item()/len(val_pred_labels))
+
+    # Adjust metrics to get average loss and accuracy per batch
+    val_loss = val_loss / len(dataloader)
+    val_acc = val_acc / len(dataloader)
+    return val_loss, val_acc
+
+# 1. Take in various parameters required for training and test steps
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          val_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5,
+          device= 'cpu',
+          model_name:str = None,
+          ):
+
+    # 2. Create empty results dictionary
+    results = {"train_loss": [],
+        "train_acc": [],
+        "val_loss": [],
+        "val_acc": []
+    }
+
+    val_acc_best = 0
+    # 3. Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer, device=device)
+        val_loss, val_acc = val_step(model=model,
+            dataloader=val_dataloader,
+            loss_fn=loss_fn, device=device)
+
+        # 4. Print out what's happening
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"val_loss: {val_loss:.4f} | "
+            f"val_acc: {val_acc:.4f} | "
+            f"lr: {val_acc:.6f} | " 
+        )
+
+        # Save best model
+        if val_acc > val_acc_best:
+            print(f'Save best model on epoch {epoch+1}')
+            torch.save(model.state_dict(), model_name)
+            val_acc_best = val_acc
+
+
+        # 5. Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["val_loss"].append(val_loss)
+        results["val_acc"].append(val_acc)
+
+    # 6. Return the filled results at the end of the epochs
+    return results
+
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "val_loss": [...],
+             "val_acc": [...]}
+    """
+
+    # Get the loss values of the results dictionary (training and test)
+    loss = results['train_loss']
+    val_loss = results['val_loss']
+
+    # Get the accuracy values of the results dictionary (training and test)
+    accuracy = results['train_acc']
+    val_accuracy = results['val_acc']
+
+    # Figure out how many epochs there were
+    epochs = range(len(results['train_loss']))
+
+    # Setup a plot
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='train_loss')
+    plt.plot(epochs, val_loss, label='val_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='train_accuracy')
+    plt.plot(epochs, val_accuracy, label='val_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend();
+
+
+
+def predict_random_image(model, test_dataloader, device, model_path):
+    state_dict = torch.load(model_path)
+    model.load_state_dict(state_dict)
+    # Set the seed for reproducibility
+    random.seed(42)
+
+    # Get a batch from test_dataloader
+    images, labels = next(iter(test_dataloader))
+
+    # Get a random image from the batch
+    random_index = random.randint(0, len(images) - 1)
+    image = images[random_index]
+    label = labels[random_index]
+
+    with torch.no_grad():
+        image = image.unsqueeze(0)
+        output = model(image.to(device))
+        probabilities = F.softmax(output, dim=1)
+        _, predicted = torch.max(output.data, 1)
+        probability = probabilities[0][predicted].item()
+
+    plt.imshow(np.transpose(image.squeeze(), (1, 2, 0)))
+    plt.title(f"Actual label: {label} | Predicted label: {predicted.item()}, Probability: {probability:.2f}")
+    plt.axis("off")
+    plt.show()
+
+
+def evaluate(model, test_dataloader, device, model_path):
+    state_dict = torch.load(model_path)
+    model.load_state_dict(state_dict)
+    # Set model to evaluation mode
+    model.eval()
+
+    # Initialize lists for true and predicted labels
+    y_true = []
+    y_pred = []
+
+    # Iterate over test data and predict labels
+    with torch.no_grad():
+        for inputs, labels in tqdm(test_dataloader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            y_true.extend(labels.tolist())
+            y_pred.extend(predicted.tolist())
+
+    return y_true, y_pred
+
+
+#Defining function for confusion matrix plot
+def plot_confusion_matrix(y_true, y_pred, classes, normalize=False, cmap=plt.cm.Blues):
+    if normalize:
+        title = 'Normalized confusion matrix'
+    else:
+        title = 'Confusion matrix, without normalization'
+
+    # Computing confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    # Visualizing
+    fig, ax = plt.subplots(figsize=(10, 10))
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+   # Rotating the tick labels and setting their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+    # Looping over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
